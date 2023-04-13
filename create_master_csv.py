71778b3c1245af8e910c6901ef3e4e3d59f9edd6
Niall Buckley <buckleyniall100@gmail.com>
2023-04-13 16:42:40 -0500

    combine all runs to one master csv

--- create_master_csv.py
+++ create_master_csv.py
@@ -18,19 +18,22 @@ try:
 except FileNotFoundError:
     open('seen.txt', 'w').close()
 
+dfs = []
 
 # Loop through each csv file and append its data to the merged data DataFrame
 for csv_file in csv_files:
     if os.path.basename(csv_file) in seen_files:
         print (f"Skipping {csv_file} - already seen!")
         continue
-    data = pd.read_csv(csv_file)
-    merged_data = merged_data.append(data, ignore_index=True)
+    print (f"Processing {csv_file}")
+    one_run = pd.read_csv(csv_file)
+    dfs.append(one_run)
     seen_files.add(os.path.basename(csv_file))
-    break
+
+combined_df = pd.concat(dfs)
 
 # Save the merged data to the master CSV file
-merged_data.to_csv('./results/master_file.csv', index=False)
+combined_df.to_csv('./results/master_file.csv', index=False)
 
 with open('seen.txt', 'w') as file:
     for f in seen_files:
