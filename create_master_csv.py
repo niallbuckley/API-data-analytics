86ecd8e9f6c6be5216e6e46776c31ee6dac4e690
Niall Buckley <buckleyniall100@gmail.com>
2023-04-18 12:14:53 -0500

    error handling creating the master csv

--- create_master_csv.py
+++ create_master_csv.py
@@ -24,16 +24,26 @@ for csv_file in csv_files:
     if os.path.basename(csv_file) in seen_files:
         print (f"Skipping {csv_file} - already seen!")
         continue
+
+    if os.path.getsize(csv_file) == 0:
+        print (f"Skipping {csv_file} - no data!")
+        seen_files.add(os.path.basename(csv_file))
+        continue
+
     print (f"Processing {csv_file}")
     one_run = pd.read_csv(csv_file)
     dfs.append(one_run)
     seen_files.add(os.path.basename(csv_file))
 
-combined_df = pd.concat(dfs)
+try:
+    combined_df = pd.concat(dfs)
 
-# Save the merged data to the master CSV file
-combined_df.to_csv('./results/master_file.csv', index=False)
+    # Save the merged data to the master CSV file
+    combined_df.to_csv('./results/master_file.csv', index=False)
+
+except ValueError:
+    print ("Did not gather any new results to add to master.csv!")
 
 with open('seen.txt', 'w') as file:
-    for f in seen_files:
-        file.write(f"{f}\n")
+        for f in seen_files:
+            file.write(f"{f}\n")
