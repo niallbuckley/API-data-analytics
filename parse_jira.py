98bd2a7f09e7fa5b71872a10cbaef8c5af454ec2
Niall Buckley <buckleyniall100@gmail.com>
2023-04-24 21:04:20 -0500

    keep track of seen keys - maintenance

--- parse_jira.py
+++ parse_jira.py
@@ -33,7 +33,7 @@ def __get_test_cycle_attachments(key):
     response = requests.get(QMetryConst.ENDP_TEST_CYCLES_BASE + key + '/attachment/', headers=QMetryConst.HEADER)
     if not response.ok:
         print ("ERROR couldn't get attachment for key: ", key, "  ", response.status_code,  " RETRY!!")
-        return
+        return 1
     
     if response.json()["total"] > 0:
         for objct in response.json()["data"]:
@@ -41,10 +41,11 @@ def __get_test_cycle_attachments(key):
                 #if seen before. continue
                 print (objct["url"], objct["id"])
                 download_zip(objct["url"], str(objct["id"]))
+    return 0
     
 
 
-def __get_test_cycle_list():
+def __get_test_cycle_list(seen_keys):
      pload = {"filter":{"projectId":"10453","folderId":521282}}
      params = '?maxResults=100'
      response = requests.post(QMetryConst.ENDP_TEST_CYCLES_BASE + 'search/' + params,  json=pload, headers=QMetryConst.HEADER)
@@ -52,7 +53,11 @@ def __get_test_cycle_list():
         count = 0
         for key_obj in response.json()["data"]:
             print ("INDEX: ", count, "  KEY: ", key_obj["key"])
-            __get_test_cycle_attachments(key_obj["key"])
+            if key_obj["key"] in seen_keys:
+                continue
+            res = __get_test_cycle_attachments(key_obj["key"])
+            if res:
+                seen_keys.add(key_obj["key"])
             count += 1
 
         # Extract all the useful data from the logs
@@ -60,6 +65,7 @@ def __get_test_cycle_list():
 
         # Delete useless logs and auto_data from directory
         delete_log_files()
+        return seen_keys
 
      else:
         print ("Error getting the list of test cycle keys response code: " + str(response.status_code))
@@ -67,4 +73,17 @@ def __get_test_cycle_list():
 
 
 if __name__ == '__main__':
-    __get_test_cycle_list()
+    seen_keys = set()
+
+    try:
+        with open('seen_keys.txt', 'r') as file:
+            for line in file:
+                seen_keys.add(line.strip())
+    except FileNotFoundError:
+        open('seen_keys.txt', 'w').close()
+
+    updated_seen_keys = __get_test_cycle_list(seen_keys)
+
+    with open('seen_keys.txt', 'w') as file:
+        for key in updated_seen_keys:
+            file.write(f"{key}\n")
