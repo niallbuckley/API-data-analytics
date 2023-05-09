90a102e01bbd89eebf9863ba21d588d13fc0b79b
Niall Buckley <buckleyniall100@gmail.com>
2023-05-09 14:15:41 -0500

    clean up bash script

--- reporting.sh
+++ reporting.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 
-# jupyter nbconvert --to notebook --execute --ExecutePreprocessor.allow_errors=True --ExecutePreprocessor.timeout=None analysis-API-endpoints.ipynb
-
 python3 parse_jira.py
 if [ $? -eq 0 ]; then
     echo "Parse jira executed successfully!"
@@ -10,4 +8,4 @@ else
     exit 1
 fi
 python3 create_master_csv.py
-#jupyter nbconvert --execute --to html --TemplateExporter.exclude_input=True automation_performance_analysis.ipynb
+jupyter nbconvert --execute --to html --TemplateExporter.exclude_input=True automation_performance_analysis.ipynb
