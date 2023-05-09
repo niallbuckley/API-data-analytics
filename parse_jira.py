7444cd756e0ad269c9a7d1d992e45cabdbd3db4e
Niall Buckley <buckleyniall100@gmail.com>
2023-05-09 14:00:42 -0500

    update the parse jira with py3 csv

--- parse_jira.py
+++ parse_jira.py
@@ -1,4 +1,4 @@
-
+import os
 import QMetryConst
 import requests
 import subprocess
@@ -20,13 +20,25 @@ def delete_log_files():
         subprocess.run(["rm", file])
 
 
-def copy_csv_files():
-    source_pattern = "./logfiles/app/stat_with_test_result-*"
+def copy_csv_file(qmetry_key):
+    file_path = "./logfiles/request_stats_new.csv"
     destination_dir = "./results/" 
+    # Extract the filename from the file path
+    filename = os.path.basename(file_path)
 
-    matching_files = glob.glob(source_pattern)
-    for file_path in matching_files:
-        shutil.copy(file_path, destination_dir)
+    # Split the filename into basename and extension
+    basename, extension = os.path.splitext(filename)
+
+    # Add qmetry key to the end of the basename
+    new_basename = basename + "-" + str(qmetry_key)
+
+    # Combine the new basename and extension to create the new filename
+    new_filename = new_basename + extension
+
+    # Join the destination directory path and the new filename
+    new_file_path = os.path.join(destination_dir, new_filename)
+
+    shutil.copy(file_path, new_file_path)
 
 
 def __get_test_cycle_attachments(key):
@@ -60,11 +72,10 @@ def __get_test_cycle_list(seen_keys):
             res = __get_test_cycle_attachments(key_obj["key"])
             if res == 0:
                 seen_keys.add(key_obj["key"])
+                # Extract all the useful data from the logs
+                copy_csv_file(key_obj["key"])
                 print (key_obj["key"], " added to seen keys!")
             count += 1
-
-        # Extract all the useful data from the logs
-        copy_csv_files()
 
         # Delete useless logs and auto_data from directory
         delete_log_files()
